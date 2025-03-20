7175da87f812020e354e13be3ca25cdced38b155
lndmri <mariolanda0707@gmail.com>
2025-03-19 20:13:38 -0400

    First version Mario 1.32x faster than original code

--- project2/src/hash.c
+++ project2/src/hash.c
@@ -2,15 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
-
+#include <stdbool.h>
+#include "dictionary.c"
+#include "dictionary.h"
 #include "hash_functions.h"
 
-#define KEEP 16 // only the first 16 bytes of a hash are kept
+// KEEP is defined in dictionary.h
+#define MAX_LEN 256
 
-struct cracked_hash {
+typedef struct cracked_hash {
 	char hash[2*KEEP+1];
 	char *password, *alg;
-};
+}cracked_hash;
 
 typedef unsigned char * (*hashing)(unsigned char *, unsigned int);
 
@@ -18,12 +21,17 @@ int n_algs = 4;
 hashing fn[4] = {calculate_md5, calculate_sha1, calculate_sha256, calculate_sha512};
 char *algs[4] = {"MD5", "SHA1", "SHA256", "SHA512"};
 
-int compare_hashes(char *a, char *b) {
-	for(int i=0; i < 2*KEEP; i++)
-		if(a[i] != b[i])
-			return 0;
-	return 1;
-}
+// abstracted hash count function to optimize it with threads
+// int count_hashes (char *hashed_list)
+// {
+// 	char hex_hash[2*KEEP+1];
+// 	int n = 0;
+// 	FILE *fp = fopen(hashed_list, "r");
+// 	assert(fp != NULL);
+// 	while(fscanf(fp, "%s", hex_hash) == 1)
+// 		n++;
+// 	return n;
+// }
 
 // Function name: crack_hashed_passwords
 // Description:   Computes different hashes for each password in the password list,
@@ -32,46 +40,42 @@ int compare_hashes(char *a, char *b) {
 //                the same hash, only the first one in the list is printed.
 void crack_hashed_passwords(char *password_list, char *hashed_list, char *output) {
 	FILE *fp;
-	char password[256];  // passwords have at most 255 characters
+	char password[MAX_LEN];  // passwords have at most 255 characters
 	char hex_hash[2*KEEP+1]; // hashed passwords have at most 'keep' characters
 
-	// load hashed passwords
-	int n_hashed = 0;
-	struct cracked_hash *cracked_hashes;
-	fp = fopen(hashed_list, "r");
-	assert(fp != NULL);
-	while(fscanf(fp, "%s", hex_hash) == 1)
-		n_hashed++;
-	rewind(fp);
-	cracked_hashes = (struct cracked_hash *) malloc(n_hashed*sizeof(struct cracked_hash));
-	assert(cracked_hashes != NULL);
-	for(int i=0; i < n_hashed; i++) {
-		fscanf(fp, "%s", cracked_hashes[i].hash);
-		cracked_hashes[i].password = NULL;
-		cracked_hashes[i].alg = NULL;
+	// load hashed passwords into hash table and file ordered linked list
+	bool loaded = load(hashed_list);
+
+	if (!loaded)
+	{
+		printf("Error loading the hash table\n");
+		return;
 	}
-	fclose(fp);
+	int n_hashed = loaded;
+	
 
 	// load common passwords, hash them, and compare them to hashed passwords
 	fp = fopen(password_list, "r");
 	assert(fp != NULL);
-	while(fscanf(fp, "%s", password) == 1) {
-
+	while(fgets(password, MAX_LEN, fp) != NULL) {	// changed fscanf function to fgets as it is faster
+		// removing the newline charcter
+		password[strcspn(password, "\n")] = '\0';
+		
 		for(int i=0; i < n_algs; i++) {
-			unsigned char *hash = fn[i]((unsigned char *)password, strlen(password));
+			unsigned char *hash_v = fn[i]((unsigned char *)password, strlen(password)); // storing the value of the output of the hash function in the hash pointer
 			for(int j=0; j < KEEP; j++)
-				sprintf(&hex_hash[2*j], "%02x", hash[j]);
+				sprintf(&hex_hash[2*j], "%02x", hash_v[j]); // converting the binary hash into hex hash
 			hex_hash[2*KEEP] = '\0';
-			for(int j=0; j < n_hashed; j++) {
-				if(cracked_hashes[j].password !=  NULL)
-					continue;
-				if(compare_hashes(hex_hash, cracked_hashes[j].hash)) {
-					cracked_hashes[j].password = strdup(password);
-					cracked_hashes[j].alg = algs[i];
-					break;
-				}
+			// printf("%s : %s\n",password, hex_hash);
+
+			
+			node *res = check(hex_hash);
+			if (res)
+			{
+				res->password = strdup(password);
+				res->alg = algs[i];
 			}
-			free(hash);
+			free(hash_v);
 		}
 	}
 	fclose(fp);
@@ -79,17 +83,17 @@ void crack_hashed_passwords(char *password_list, char *hashed_list, char *output
 	// print results
 	fp = fopen(output, "w");
 	assert(fp != NULL);
-	for(int i=0; i < n_hashed; i++) {
-		if(cracked_hashes[i].password ==  NULL)
+	node *p;
+	for(p = head; p != NULL; p = p->next_in_file) {
+		if(p->password ==  NULL)
 			fprintf(fp, "not found\n");
 		else
-			fprintf(fp, "%s:%s\n", cracked_hashes[i].password, cracked_hashes[i].alg);
+			fprintf(fp, "%s:%s\n", p->password, p->alg);
 	}
+
 	fclose(fp);
 
 	// release stuff
-	for(int i=0; i < n_hashed; i++)
-		free(cracked_hashes[i].password);
-	free(cracked_hashes);
+	unload();
 }
 
